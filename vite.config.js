2d64bf4dfc3277124d9f72e9682a9ce2777149b4
Rauolli <rauol@t-online.de>
2025-03-10 20:12:03 +0100

    changed to a pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Time Converter',
+        short_name: 'TimeConverter',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        description: 'A simple time converter',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
