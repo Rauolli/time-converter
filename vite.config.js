71708bf2c5f8fa94122fddedcb6463044c4acf8a
Rauolli <rauol@t-online.de>
2025-03-11 16:49:31 +0100

    changed from pwa back again -> because of issues

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Time Converter',
-        short_name: 'TimeConverter',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        description: 'A simple time converter',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [svelte()],
+  base: '/timeconverter/'
 })
